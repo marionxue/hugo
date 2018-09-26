987b22afcbbd6ab28430f9dfab06daaa2d3cf466
yangchuansheng <yangchuansheng33@gmail.com>
2018-09-26 17:50:24 +0800

    first commit

--- restart_hugo.sh
+++ restart_hugo.sh
@@ -3,7 +3,9 @@
 sh publish_hugo.sh
 rm -rf public/about/
 hugo-algolia -s
-docker restart hugo
+#docker restart hugo
+docker-compose restart hugo
+docker-compose restart service-envoy
 git add --all *
 git commit -m "first commit"
 git push -u origin master
